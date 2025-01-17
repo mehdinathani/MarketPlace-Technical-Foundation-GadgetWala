0c6c46e453da1653745277d337b3c463834ebeee
mehdinathani <mehdinathani@gmail.com>
2025-01-17 09:00:21 +0500

    first commit

---
+++ shipment.ts
@@ -0,0 +1,28 @@
+export const shipment = {
+    name: "shipment",
+    type: "document",
+    title: "shipment",
+    fields: [
+        {
+            name: "orderId",
+            type: "string",
+            title: "orderId"
+        },
+
+        {
+            name: "shipmentName",
+            type: "string",
+            title: "shipmentName"
+        },
+        {
+            name: "status",
+            type: "string",
+            title: "status"
+        },
+        {
+            name: "shipmentTime",
+            type: "string",
+            title: "shipmentTime"
+        }
+    ]
+}
