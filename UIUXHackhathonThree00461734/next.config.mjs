51f45022f30da855d5cd5ebf42e3901019804376
mehdinathani <mehdinathani@gmail.com>
2025-01-21 22:14:59 +0500

    update dependencies 4

--- UIUXHackhathonThree00461734/next.config.mjs
+++ UIUXHackhathonThree00461734/next.config.mjs
@@ -11,10 +11,10 @@ const nextConfig = {
             }
         ]
     },
-    eslint: {
-        ignoreDuringBuilds: true,
+    // eslint: {
+    //     ignoreDuringBuilds: true,
 
-    }
+    // }
 
 };
 
